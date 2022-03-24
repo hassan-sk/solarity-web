7124842a35dff8f75d7f666f66c690971446c7c6
Muhammad Hassan <hassan24sk@gmail.com>
2022-03-25 01:54:49 +0500

    MERGING

--- src/components/WalletButton/index.tsx
+++ src/components/WalletButton/index.tsx
@@ -8,7 +8,7 @@ const ButtonWallet = () => {
   return (
     <Link href="/profile" passHref>
       <a className="gap-3 pr-1 font-normal normal-case btn rounded-3xl btn-secondary ">
-        <span>Wallet 13...8x </span>
+        <span>Wallet 21...8x </span>
         <div className="w-[1px]  h-2/3 bg-[#5153F0]" />
         <Image src={AavatartPlaceholder} alt="user avatar" />
       </a>
