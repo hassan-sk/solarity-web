48d963ae76b4c49f98071897c395d57d7a85424e
fullstackdev0410 <fullstackdev0410@outlook.com>
2022-04-08 21:58:00 +0200

    changed AframeEditRoom.

--- src/components/AframeEditRoom/index.js
+++ src/components/AframeEditRoom/index.js
@@ -110,7 +110,7 @@ export default function AframeEditRoom({
             ></a-asset-item>
             <a-asset-item
               id="chair-gltf"
-              src="/assets/models/chair.glb"
+              src="/assets/models/Chair.glb"
             ></a-asset-item>
 
             <a-asset-item
