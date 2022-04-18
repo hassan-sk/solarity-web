717cdfb9ee14024c99f1571f75fe2afe59e35efb
Muhammad Hassan <hassan24sk@gmail.com>
2022-04-18 22:22:51 +0500

    Fixed another bug

--- src/pages/experience/invitation/[link].tsx
+++ src/pages/experience/invitation/[link].tsx
@@ -1,77 +1,93 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import React, { FC, useEffect } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import Layout from "components/Layout";
 import Base from "components/Modals/Base";
 import Hero from "modules/User/Hero";
 import Home from "modules/User/Home";
 import RightSidebar from "modules/User/Sidebar";
-import { getServerSideProps, InvitationPageProps } from "modules/experience/Invitation";
-import NoInvitationView from "modules/experience/NoInvitationView";
+import {
+  getServerSideProps,
+  InvitationPageProps,
+} from "modules/Experience/Invitation";
+import NoInvitationView from "modules/Experience/NoInvitationView";
 import { Xicon, Revert, Accept } from "components/Icons";
-import { setModel, setName, addPeer, addMsg, removePeer, setRooms, setMsg } from '../../../redux/slices/chatSlice';
-import ACTIONS from '../../../config/actions';
+import {
+  setModel,
+  setName,
+  addPeer,
+  addMsg,
+  removePeer,
+  setRooms,
+  setMsg,
+} from "../../../redux/slices/chatSlice";
+import ACTIONS from "../../../config/actions";
 const ProfileIndex: FC<InvitationPageProps> = ({ invitation, success }) => {
   const dispatch = useDispatch();
   const router = useRouter();
   if (!success) return <NoInvitationView />;
   useEffect(() => {
     // When a user click f5 key, it helps to forget a user's name.
-    if(localStorage.getItem('name')) {
-      dispatch(setName(localStorage.getItem('name')));
+    if (localStorage.getItem("name")) {
+      dispatch(setName(localStorage.getItem("name")));
     }
-    
+
     // This part is main for socket.
-    if(!window.socket) {
+    if (!window.socket) {
       return;
     }
-    if(!window.listen) {
-      window.socket.on(ACTIONS.ADD_PEER, data => {
+    if (!window.listen) {
+      window.socket.on(ACTIONS.ADD_PEER, (data) => {
         dispatch(addPeer(data));
-      })
+      });
       window.socket.on(ACTIONS.SEND_MSG, (data: any) => {
         dispatch(addMsg(data));
-      })
-      window.socket.on(ACTIONS.REMOVE_PEER, data => {
+      });
+      window.socket.on(ACTIONS.REMOVE_PEER, (data) => {
         dispatch(removePeer(data));
-      })
+      });
 
-      window.socket.on(ACTIONS.ROOM_LIST, data => {
+      window.socket.on(ACTIONS.ROOM_LIST, (data) => {
         dispatch(setRooms(data.rooms));
-      })
+      });
 
-      window.socket.on(ACTIONS.CREATE_ROOM, data => {
+      window.socket.on(ACTIONS.CREATE_ROOM, (data) => {
         dispatch(setMsg(data.msgs));
-      })
+      });
 
-      window.socket.on(ACTIONS.ROOM_READY, data => {
+      window.socket.on(ACTIONS.ROOM_READY, (data) => {
         router.push(`experience/room?rid=${data.roomId}`);
-      })
+      });
       window.listen = true;
     }
 
     window.socket.emit(ACTIONS.ROOM_LIST, {});
-  }, [])
+  }, []);
   const accept = () => {
-    if(!!window.socket){
-      window.socket.emit(ACTIONS.ACEEPT_INVITATION, {roomId: invitation.roomId, username: invitation.name});
+    if (!!window.socket) {
+      window.socket.emit(ACTIONS.ACEEPT_INVITATION, {
+        roomId: invitation.roomId,
+        username: invitation.name,
+      });
       dispatch(setModel(1));
       dispatch(setName(invitation.name));
-      router.push('/experience/room?rid=' + invitation.roomId);
+      router.push("/experience/room?rid=" + invitation.roomId);
     }
-  }
+  };
 
   const deny = () => {
-    if(!!window.socket){
-      window.socket.emit(ACTIONS.ACEEPT_INVITATION, {roomId: invitation.roomId, username: invitation.name});
+    if (!!window.socket) {
+      window.socket.emit(ACTIONS.ACEEPT_INVITATION, {
+        roomId: invitation.roomId,
+        username: invitation.name,
+      });
     }
-    router.push('/');
-  }
+    router.push("/");
+  };
 
   const back = () => {
-    router.push('/');
-
-  }
+    router.push("/");
+  };
   return (
     <div className="absolute top-[30vh] left-[70vh] w-[60vh]">
       <div className="bg-brandblack rounded-3xl w-full">
@@ -81,11 +97,15 @@ const ProfileIndex: FC<InvitationPageProps> = ({ invitation, success }) => {
           </div>
           <div className="flex justify-between mb-5">
             <span className="font-semibold">Room Name</span>
-            <span className="font-thin text-secondary">{invitation.roomName}</span>
+            <span className="font-thin text-secondary">
+              {invitation.roomName}
+            </span>
           </div>
           <div className="flex justify-between mb-5">
             <span className="font-semibold">Room Number</span>
-            <span className="font-thin text-secondary">{invitation.roomId}</span>
+            <span className="font-thin text-secondary">
+              {invitation.roomId}
+            </span>
           </div>
           <div className="flex justify-between mb-5">
             <span className="font-semibold">Invited By</span>
@@ -95,19 +115,28 @@ const ProfileIndex: FC<InvitationPageProps> = ({ invitation, success }) => {
             <div>
               <div className="text-secondary">The Invitation is expired.</div>
               <div className="w-full flex justify-end">
-                <button className="gap-2 text-xs normal-case rounded-full btn btn-primary px-6" onClick={back}>
+                <button
+                  className="gap-2 text-xs normal-case rounded-full btn btn-primary px-6"
+                  onClick={back}
+                >
                   <Revert />
                   Back
                 </button>
               </div>
             </div>
-          ): (
+          ) : (
             <div className="flex gap-4 justify-between px-16">
-              <button className="gap-2 text-xs normal-case rounded-full btn btn-primary px-6" onClick={accept}>
+              <button
+                className="gap-2 text-xs normal-case rounded-full btn btn-primary px-6"
+                onClick={accept}
+              >
                 <Accept />
                 Accept
               </button>
-              <button className="gap-2 text-xs normal-case rounded-full btn btn-primary px-6" onClick={deny}>
+              <button
+                className="gap-2 text-xs normal-case rounded-full btn btn-primary px-6"
+                onClick={deny}
+              >
                 <Xicon />
                 Deny
               </button>
